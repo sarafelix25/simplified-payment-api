6c44042cd2128f3cd5de5cea2560ae2039e6f0b5
Sara Felix <70541669+sarafelix25@users.noreply.github.com>
2024-09-17 00:12:12 +0000

    unit tests check for repository

--- src/test/java/com/picpaysimplificado/repositories/UserRepositoryTest.java
+++ src/test/java/com/picpaysimplificado/repositories/UserRepositoryTest.java
@@ -1,9 +1,9 @@
 package com.picpaysimplificado.repositories;
 
-import com.picpaysimplificado.domain.user.User;
-import com.picpaysimplificado.domain.user.UserType;
-import com.picpaysimplificado.dtos.UserDTO;
-import jakarta.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import org.hibernate.usertype.UserType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +11,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.math.BigDecimal;
-import java.util.Optional;
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.dtos.UserDTO;
 
-import static org.junit.jupiter.api.Assertions.*;
+import jakarta.persistence.EntityManager;
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -27,28 +27,28 @@ class UserRepositoryTest {
     EntityManager entityManager;
 
     @Test
-    @DisplayName("Should get User successfully from DB")
-    void findUserByDocumentCase1() {
-        String document = "99999999901";
-        UserDTO data = new UserDTO("Fernanda", "Teste", document, new BigDecimal(10), "test@gmail.com", "44444", UserType.COMMON);
+    @DisplayName("Should get user successfully from DB")
+    public void findUserByDocument() {
+        String document = "12345678";
+        UserDTO data = new UserDTO("Sara", "Test", document, new BigDecimal(24), "sara@email.com", "123456", com.picpaysimplificado.domain.user.UserType.COMMON);
         this.createUser(data);
-
+        
         Optional<User> result = this.userRepository.findUserByDocument(document);
-
+    
         assertThat(result.isPresent()).isTrue();
     }
 
     @Test
-    @DisplayName("Should not get User from DB when user not exists")
-    void findUserByDocumentCase2() {
-        String document = "99999999901";
-
+    @DisplayName("Should not get user User from DB when user not exists")
+    public void findUserByDocumentNotFound() {
+        String document = "0001";
+      
         Optional<User> result = this.userRepository.findUserByDocument(document);
-
+    
         assertThat(result.isEmpty()).isTrue();
     }
 
-    private User createUser(UserDTO data){
+    private User createUser(UserDTO data) {
         User newUser = new User(data);
         this.entityManager.persist(newUser);
         return newUser;
